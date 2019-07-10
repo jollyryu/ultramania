455bfe1d3dce02dec8cf3a6fa74045fbf9a23397
jollyryu <jollyryu@naver.com>
2019-07-10 14:46:31 +0900

    Update ServiceController.java

--- src/main/java/com/ryu/ultramania/controller/ServiceController.java
+++ src/main/java/com/ryu/ultramania/controller/ServiceController.java
@@ -3,7 +3,7 @@ package com.ryu.ultramania.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//주석테스트
 @Controller
 public class ServiceController {
 
